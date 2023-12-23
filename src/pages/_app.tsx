1ff03d3a6d88675c8d5514d1f451f41ce8448c36
haqqisadieda <haqqisadiedapandega@gmail.com>
2023-12-23 19:50:16 +0700

    feat: setup admin layout and middleware

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,28 @@
-import Navbar from '@/components/layouts/Navbar';
-import '@/styles/globals.css';
-import { SessionProvider } from 'next-auth/react';
-import type { AppProps } from 'next/app';
-import { Lato } from 'next/font/google';
+import Navbar from "@/components/fragments/Navbar";
+import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import { Lato } from "next/font/google";
+import { useRouter } from "next/router";
 
 const lato = Lato({
-    subsets: ['latin'],
-    weight: ['100', '300', '400', '700', '900'],
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
 });
 
+const disableNavbar = ["auth", "admin"];
+
 export default function App({
-    Component,
-    pageProps: { session, ...pageProps },
+  Component,
+  pageProps: { session, ...pageProps },
 }: AppProps) {
-    return (
-        <SessionProvider session={session}>
-            <div className={lato.className}>
-                <Navbar />
-                <Component {...pageProps} />;
-            </div>
-        </SessionProvider>
-    );
+  const { pathname } = useRouter();
+  return (
+    <SessionProvider session={session}>
+      <div className={lato.className}>
+        {!disableNavbar.includes(pathname.split("/")[1]) && <Navbar />}
+        <Component {...pageProps} />;
+      </div>
+    </SessionProvider>
+  );
 }
---
+++ src/pages/admin/users/index.tsx
@@ -0,0 +1,9 @@
+const AdminUsersPage = () => {
+  return (
+    <div>
+      <div>Admin User Page</div>
+    </div>
+  );
+};
+
+export default AdminUsersPage;
